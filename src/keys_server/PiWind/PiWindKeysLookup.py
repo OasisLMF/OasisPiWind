5e261b86a2a8a93c8963251817777131c7120709
Sandeep Murthy <smurthy@protonmail.ch>
2018-06-03 21:40:13 +0100

    Refactored and optimised PiWind keys lookup - speedup achieved by using RTree index for the point-poly area peril lookup + add RTree to PiWind requirements

--- src/keys_server/PiWind/PiWindKeysLookup.py
+++ src/keys_server/PiWind/PiWindKeysLookup.py
@@ -9,6 +9,8 @@ import io
 import logging
 import os
 
+from collections import OrderedDict
+
 try:
      from ConfigParser import ConfigParser
 except ImportError:
@@ -17,6 +19,9 @@ except ImportError:
 # Python 2 non-standard library imports
 import pandas as pd
 import six
+
+from rtree.core import RTreeError
+from rtree.index import Index
 
 from shapely.geometry import (
     Point,
@@ -78,23 +83,22 @@ class PiWindKeysLookup(OasisBaseKeysLookup):
             model_version
         )
 
-        # Load the area perils dictionary file into a dict
+        # Load the area perils dictionary file into a dict and create an RTree index
         self.area_perils_file_path = os.path.join(self.keys_data_directory, self.config.get('Keys Data', 'AREA_PERILS_FILE'))
+
+        self.area_perils = {}
+        self.area_perils_idx = Index()
+
         with io.open(self.area_perils_file_path, 'r', encoding='utf-8') as f:
             ap_df = pd.read_csv(f, float_precision='high')
             ap_df = ap_df.where(ap_df.notnull(), None)
             ap_df.columns = ap_df.columns.str.lower()
-            
-            # Area perils lookup dictionary - keys are tuples of lat/lon coordinates for polygon vertices
-            # and values are contained in a dictionary containing the polygon (`shapely.geometry.MultiPoint`)
-            # represented by the vertices and the area peril ID associated with that polygon
-            self.area_perils = {
-                tuple((it['lat{}'.format(i)], it['lon{}'.format(i)]) for i in range(1, 5)):{
-                    'polygon':MultiPoint(tuple((it['lat{}'.format(i)], it['lon{}'.format(i)]) for i in range(1, 5))).convex_hull,
-                    'area_peril_id': int(it['area_peril_id'])
-                }
-                for _, it in ap_df.iterrows()
-            }
+
+            for _, it in ap_df.iterrows():
+                apid = int(it['area_peril_id'])
+                poly = MultiPoint(tuple((float(it['lat{}'.format(i)]),float(it['lon{}'.format(i)])) for i in range(1, 5))).convex_hull
+                self.area_perils[apid] = poly
+                self.area_perils_idx.insert(apid, poly.bounds)
 
         # Load the vulnerability dictionary file into a dict
         self.vulnerabilities_file_path = os.path.join(self.keys_data_directory, self.config.get('Keys Data', 'VULNERABILITIES_FILE'))
@@ -102,52 +106,108 @@ class PiWindKeysLookup(OasisBaseKeysLookup):
             vln_df = pd.read_csv(f, float_precision='high')
             vln_df = vln_df.where(vln_df.notnull(), None)
             vln_df.columns = vln_df.columns.str.lower()
-            
-            # Vulnerabilities dictionary - keys are coverage/class 1 tuples and values are vulnerability IDs
-            self.vulnerabilities = {
-                (int(it['coverage']), it['class_1']):int(it['vulnerability_id'])
-                for _, it in vln_df.iterrows()
-            }
 
-    def get_area_peril_id(self, loc_it):
+            # Vulnerabilities dictionary - keys are coverage/class 1 tuples and values are vulnerability IDs
+            self.vulnerabilities = OrderedDict(
+                [
+                    ((int(it['coverage']),it['class_1']), int(it['vulnerability_id']))
+                    for _, it in vln_df.iterrows()
+                ]
+            )
+
+    def lookup_area_peril(self, loc_item):
         """
-        Area peril lookup method for an individual item, which could be a dict or a
+        Area peril lookup for an individual item, which could be a dict or a
         Pandas series.
         """
+        lat = loc_item['lat']
+        lon = loc_item['lon']
+
+        ap_result = lambda loc_id, apst, apid, apmsg: {
+            'id': loc_id,
+            'area_peril_status': apst,
+            'area_peril_id': apid,
+            'area_peril_message': apmsg
+        }
+
+        try:
+            lat = float(lat)
+            lon = float(lon)
+            Point(float(lat), float(lon))
+            if not ((-90 <= lat <= 90) and (-180 <= lon <= 180)):
+                raise ValueError('lon/lat out of bounds')
+        except (ValueError, TypeError) as e:
+            return ap_result(loc_item['id'], KEYS_STATUS_FAIL, None, 'Area peril lookup: invalid lat/lon ({}, {}) - {}'.format(lat, lon, str(e)))
+
         apst = KEYS_STATUS_NOMATCH
         apmsg = 'No area peril match'
         apid = None
 
-        if not (-90 <= float(loc_it['lat']) <= 90 and -180 <= float(loc_it['lon']) <= 180):
-            apst = KEYS_STATUS_FAIL
-            apmsg = "Invalid lat/lon"
+        try:
+            apid = list(self.area_perils_idx.intersection([lat, lon]))[0]
+        except IndexError:
+            pass
+        except RTreeError as e:
+            return ap_result(loc_item['id'], KEYS_STATUS_FAIL, None, str(e))
         else:
-            for apit in six.itervalues(self.area_perils):
-                if apit['polygon'].intersects(Point(loc_it['lat'],loc_it['lon'])):
-                    apid = apit['area_peril_id']
-                    apst = KEYS_STATUS_SUCCESS
-                    apmsg = "Successful area peril match {}".format(apid)
-                    break
+            apst = KEYS_STATUS_SUCCESS
+            apmsg = 'Successful area peril lookup: {}'.format(apid)
 
-        return {'area_peril_status': apst, 'area_peril_id': apid, 'area_peril_message': apmsg}
+        return ap_result(loc_item['id'], apst, apid, apmsg)
 
-    def get_vulnerability_id(self, loc_it):
+    def lookup_vulnerability(self, loc_item):
         """
-        Vulnerability lookup method for an individual item, which could be a dict or a
+        Vulnerability lookup for an individual item, which could be a dict or a
         Pandas series.
         """
+        coverage = loc_item['coverage']
+        class_1 = loc_item['class_1']
+
+        vln_result = lambda loc_id, vlnst, vlnid, vlnmsg: {
+            'id': loc_id,
+            'vulnerability_status': vlnst,
+            'vulnerability_id': vlnid,
+            'vulnerability_message': vlnmsg
+        }
+
+        try:
+            int(coverage) and str(class_1)
+        except (TypeError, ValueError):
+            return vln_result(loc_item['id'], KEYS_STATUS_FAIL, None, 'Vulnerability lookup: invalid location coverage or class 1')
+
         vlnst = KEYS_STATUS_NOMATCH
         vlnmsg = 'No vulnerability match'
         vlnid = None
 
-        for (coverage, class_1), vid in six.iteritems(self.vulnerabilities):
-            if coverage == loc_it['coverage'] and class_1 == loc_it['class_1']:
-                vlnst = KEYS_STATUS_SUCCESS
-                vlnid = vid
-                vlnmsg = "Successful vulnerability match {}".format(vlnid)
-                break
+        try:
+            vlnid = self.vulnerabilities[(loc_item['coverage'], loc_item['class_1'])]
+        except KeyError:
+            pass
+        else:
+            vlnst = KEYS_STATUS_SUCCESS
+            vlnmsg = 'Successful vulnerability lookup: {}'.format(vlnid)
 
-        return {'vulnerability_status': vlnst, 'vulnerability_id': vlnid, 'vulnerability_message': vlnmsg}
+        return vln_result(loc_item['id'], vlnst, vlnid, vlnmsg)
+
+    def lookup_status(self, loc_item):
+        """
+        Gets the combined area peril and vulnerability lookup status and message
+        """
+        apst = loc_item['area_peril_status']
+        apmsg = loc_item['area_peril_message']
+
+        vlnst = loc_item['vulnerability_status']
+        vlnmsg = loc_item['vulnerability_message']
+
+        cmbst = KEYS_STATUS_NOMATCH
+        cmbmsg = '{}; {}'.format(apmsg, vlnmsg)
+
+        if apst == vlnst == KEYS_STATUS_SUCCESS:
+            cmbst = KEYS_STATUS_SUCCESS
+        elif apst == KEYS_STATUS_FAIL or vlnst == KEYS_STATUS_FAIL:
+            cmbst = KEYS_STATUS_FAIL
+
+        return {'status': cmbst, 'message': cmbmsg}
 
     @oasis_log()
     def process_locations(self, loc_df):
@@ -157,62 +217,30 @@ class PiWindKeysLookup(OasisBaseKeysLookup):
         NOTE: Does not return anything, but generates lookup dicts/records using `yield`.
         This method is designed to be a Python generator.
         """
-        
-        # Main loop over location items in locations dataframe
-        for _, loc_it in loc_df.iterrows():
-
-            # Initial area peril lookup values
-            apst = KEYS_STATUS_NOMATCH
-            apmsg = 'No area peril match'
-            apid = None
-
-            # Initial vulnerability lookup values
-            vlnst = KEYS_STATUS_NOMATCH
-            vlnmsg = 'No vulnerability match'
-            vlnid = None
-
-            # Combined (area peril and vulnerability) status and message
-            cmbst = KEYS_STATUS_NOMATCH
-            cmbmsg = '{}, {}'.format(apmsg, vlnmsg)
+        for _, it in loc_df.iterrows():
+            ap_lookup = self.lookup_area_peril(it)
+            apst = ap_lookup['area_peril_status']
+            apmsg = ap_lookup['area_peril_message']
             
-            # Area peril lookup for location item - could be done via function call to get_area_peril_id
-            # but it is faster to avoid function calls in a `for` loop if possible
-            if not (-90 <= float(loc_it['lat']) <= 90 and -180 <= float(loc_it['lon']) <= 180):
-                apst = KEYS_STATUS_FAIL
-                apmsg = "Invalid lat/lon"
-            else:
-                for apit in six.itervalues(self.area_perils):
-                    if apit['polygon'].intersects(Point(loc_it['lat'],loc_it['lon'])):
-                        apid = apit['area_peril_id']
-                        apst = KEYS_STATUS_SUCCESS
-                        apmsg = "Successful area peril match {}".format(apid)
-                        break
+            vln_lookup = self.lookup_vulnerability(it)
+            vlnst = vln_lookup['vulnerability_status']
+            vlnmsg = vln_lookup['vulnerability_message']
+            
+            # Could optionally call the status lookup method, but it is always
+            # better to avoid outside function calls in a `for` loop if possible
+            status = (
+                KEYS_STATUS_SUCCESS if apst == vlnst == KEYS_STATUS_SUCCESS
+                else (KEYS_STATUS_FAIL if (apst == KEYS_STATUS_FAIL or vlnst == KEYS_STATUS_FAIL) else KEYS_STATUS_NOMATCH)
+            )
+            
+            message = '{}; {}'.format(apmsg, vlnmsg)
 
-            # Vulnerablity lookup for location item - could be done via function call to get_vulnerability_id
-            # but it is faster to avoid function calls in a `for` loop if possible
-            for (coverage, class_1), vid in six.iteritems(self.vulnerabilities):
-                if coverage == loc_it['coverage'] and class_1 == loc_it['class_1']:
-                    vlnst = KEYS_STATUS_SUCCESS
-                    vlnid = vid
-                    vlnmsg = "Successful vulnerability match {}".format(vlnid)
-                    break
-
-            # Set combined status
-            if apst == KEYS_STATUS_SUCCESS and vlnst == KEYS_STATUS_SUCCESS:
-                cmbst = KEYS_STATUS_SUCCESS
-            elif apst == KEYS_STATUS_FAIL or vlnst == KEYS_STATUS_FAIL:
-                cmbst = KEYS_STATUS_FAIL
-
-            # Set combined message
-            cmbmsg = '{}, {}'.format(apmsg, vlnmsg)
-
-            # Generate lookup record
             yield {
-                "id": loc_it['id'],
-                "peril_id": self.peril_id,
-                "coverage": loc_it['coverage'],
-                "area_peril_id": apid,
-                "vulnerability_id": vlnid,
-                "message": cmbmsg,
-                "status": cmbst
+                'id': it['id'],
+                'peril_id': PERIL_ID_WIND,
+                'coverage': it['coverage'],
+                'area_peril_id': ap_lookup['area_peril_id'],
+                'vulnerability_id': vln_lookup['vulnerability_id'],
+                'status': status,
+                'message': message
             }
