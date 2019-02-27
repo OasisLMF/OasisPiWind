6cbc2e99937f1fc4595672655a1531dbf0882a16
Sam Gamble <hexadessa@gmail.com>
2019-02-27 18:26:58 +0000

    Run notebook docker in detached mode

--- notebook-launch.sh
+++ notebook-launch.sh
@@ -2,4 +2,4 @@
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 docker build -f docker/Dockerfile.jupyter -t piwind-jupyter .
-docker run  -v "$SCRIPT_DIR":/home/run -e JUPYTER_PASS='<ADD-PASSWORD-HERE>' -p 8888:8888 piwind-jupyter
+docker run  -v "$SCRIPT_DIR":/home/run -e JUPYTER_PASS='<ADD-PASSWORD-HERE>' -p 8888:8888 -d piwind-jupyter
