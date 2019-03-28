8e2573d520b9b11740462712444bf50007dc4448
Sam Gamble <hexadessa@gmail.com>
2019-03-28 16:52:00 +0000

    Update build script

--- jenkins/piwind.groovy
+++ jenkins/piwind.groovy
@@ -115,7 +115,7 @@ node {
             dir(build_workspace) {
                 sh "sed -i 's/FROM.*/FROM python:3.6/g' docker/Dockerfile.mdk-tester"
                 sh 'docker build -f docker/Dockerfile.mdk-tester -t mdk-runner:3.6 .'
-                sh "docker run mdk-runner:3.6 --model-repo-branch ${model_branch} --mdk-repo-branch ${MDK_BRANCH} --model-run-mode ${MDK_RUN}"
+                sh "docker run mdk-runner:3.6 --model-repo-branch ${MDK_MODEL} --mdk-repo-branch ${MDK_BRANCH} --model-run-mode ${MDK_RUN}"
 
             }
         }
