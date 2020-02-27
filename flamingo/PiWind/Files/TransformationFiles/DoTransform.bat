9d1bc328242d4751e37b11f3e62ad531b6892c2c
sambles <sambles@users.noreply.github.com>
2020-02-27 13:01:31 +0000

    Fix/59 deprecated structures (#60)
    * Clean up older files
    * Restore apt.txt for binder

--- flamingo/PiWind/Files/TransformationFiles/DoTransform.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-
-RaptorXML xslt --xslt-version=1 --input="OED_CanLocB.xml" --output="../ValidationFiles/piwind_modelloc.xml" --xml-validation-error-as-warning=true %* "MappingMapToOED_piwind_modelloc.xslt"
-IF ERRORLEVEL 1 EXIT/B %ERRORLEVEL%
