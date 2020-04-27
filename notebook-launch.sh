04b1f7f9b6a5b11a48033eaac907fe006555e7be
sambles <sambles@users.noreply.github.com>
2020-04-27 16:03:51 +0100

    Clean (#63)
    * clean repository
    
    * removed chunksize option in oasislmf.json
    
    * added "ri_output": true default analysis_settings
    
    * added jupyter
    
    * repoint binder to clean branch
    
    * added jupyter notebook
    
    * requirements for binder
    
    * apt for binder
    
    * binder directory and git tidy up
    
    * jupyter fixes
    
    * jupyter fixes
    
    * model data column tidy up
    
    * jupyter gridcells
    
    * jupyter gridcells
    
    * jupyter gridcells fix
    
    * Add CI test files to tests/ci
    
    * Update model settings
    
    * Update CI tests
    
    * Update model_settings.json
    
    * Fix data location
    
    * Move testing config
    
    * Fix test config
    
    * Add option to use last release worker
    
    Co-authored-by: benhayes21 <ben.hayes@oasislmf.org>

--- notebook-launch.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash                                                                                                                                                                                    
-
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-docker build -f docker/Dockerfile.jupyter -t piwind-jupyter .
-docker run  -v "$SCRIPT_DIR":/home/run -e JUPYTER_PASS='<ADD-PASSWORD-HERE>' -p 8888:8888 -d piwind-jupyter
