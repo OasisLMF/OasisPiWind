a68c673f1f87a55796f512946ec0485abee967ac
sam gamble <hexadessa@gmail.com>
2020-05-04 14:53:52 +0100

    Fix exposure_pre_analysis_example

--- src/exposure_modification/exposure_pre_analysis_example.py
+++ src/exposure_modification/exposure_pre_analysis_example.py
@@ -12,6 +12,5 @@ class ExposurePreAnalysis:
 
     def run(self):
         panda_df = pd.read_csv(self.raw_oed_location_csv, memory_map=True)
-        import ipdb; ipdb.set_trace()
-        panda_df['BuildingTIV'] = panda_df['BuildingTIV'] * self.exposure_pre_analysis_setting['BuildingTIV_multiplyer']
+        panda_df['BuildingTIV_new'] = panda_df['BuildingTIV'] * self.exposure_pre_analysis_setting['BuildingTIV_multiplyer']
         panda_df.to_csv(self.oed_location_csv, index=False)
