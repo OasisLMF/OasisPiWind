7c8bfb44d2bde150b68c69bce08147551b96e3e7
sstruzik <stephane.struzik@gmail.com>
2021-05-21 13:52:36 +0100

    piwind with new key lookup

---
+++ keys_data/PiWind/area_peril_dict_converter.py
@@ -0,0 +1,24 @@
+from shapely.geometry import Polygon
+import geopandas as gpd
+import pandas as pd
+import os
+
+df = pd.read_csv(os.path.join(os.path.dirname(__file__), "areaperil_dict.csv"))
+df.rename(columns={column:column.lower() for column in df.columns}, inplace=True)
+
+# remove duplicate for coverage
+df.drop(df[df['coverage_type'] != 1].index, inplace=True)
+df.drop(columns=['coverage_type'], inplace=True)
+
+polygon_point_order = [1, 2, 4, 3]
+
+# create the GeoDataFrame and its geometry
+gdf_peril_area = gpd.GeoDataFrame(df)
+gdf_peril_area["geometry"] = gdf_peril_area.apply(
+    lambda row: Polygon([(row[f"lon{i}"], row[f"lat{i}"]) for i in polygon_point_order]), axis=1)
+
+# remove unused coordinate
+gdf_peril_area.drop(columns=sum(([f"lon{i}", f"lat{i}"] for i in polygon_point_order), []), inplace=True)
+
+# store to parquet format
+gdf_peril_area.to_parquet(os.path.join(os.path.dirname(__file__), "areaperil_dict.parquet"))
