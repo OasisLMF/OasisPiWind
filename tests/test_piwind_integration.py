2ab1e1cda4d1627a652c6429f3be88f537c3d275
Sam Gamble <hexadessa@gmail.com>
2022-11-27 12:47:54 +0000

    Fix file list for case_8

--- tests/test_piwind_integration.py
+++ tests/test_piwind_integration.py
@@ -364,7 +364,7 @@ class case_7(TestPiWind):
 
 class case_8(TestPiWind):
     #__test__ = False  # skip this test until updated
-    expected_files = glob.glob(f"{exp_case_0}/output/*")
+    expected_files = glob.glob(f"{exp_case_8}/output/*")
 
     @classmethod
     def setUpClass(cls):
